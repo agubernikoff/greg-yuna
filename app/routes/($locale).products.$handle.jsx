6fc03b2cf47eb1a4a32438fe27cc894ffbabd984
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-06 13:15:31 -0400

    mobile pdp

--- app/routes/($locale).products.$handle.jsx
+++ app/routes/($locale).products.$handle.jsx
@@ -126,11 +126,19 @@ export default function Product() {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth < 500);
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
     <>
       <div className="product">
-        <div className="product-images">{productImage}</div>
-        <div className="product-main">
+        {isMobile ? (
           <div className="padded-filter-div full-border breadcrumbs">
             <>
               <NavLink className="crumb" to="/">
@@ -152,6 +160,32 @@ export default function Product() {
               </span>
             </>
           </div>
+        ) : null}
+        <div className="product-images">{productImage}</div>
+        <div className="product-main">
+          {isMobile ? null : (
+            <div className="padded-filter-div full-border breadcrumbs">
+              <>
+                <NavLink className="crumb" to="/">
+                  Home
+                </NavLink>
+                {' → '}
+                {product.collections.nodes[0] && state !== '/' ? (
+                  <>
+                    <NavLink className="crumb" to={to}>
+                      {state
+                        ? capitalizeFirstLetter(state.split('/collections/')[1])
+                        : product.collections.nodes[0].title}
+                    </NavLink>
+                    {' → '}
+                  </>
+                ) : null}
+                <span className="crumb" sty>
+                  {title}
+                </span>
+              </>
+            </div>
+          )}
 
           <div className="product-main-details">
             <div className="product-title-price">
