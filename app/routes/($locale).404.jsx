4dc92e08e3ccadd9b0b425e66638721b545d774c
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-06-11 16:45:48 -0400

    404

--- app/routes/($locale).404.jsx
+++ app/routes/($locale).404.jsx
@@ -4,10 +4,10 @@ export default function fourohfour() {
   return (
     <div className="route-error">
       <h2>404</h2>
-      <div>
-        <p>we're sorry, the page you're looking for doesn't exist</p>
-        <NavLink to="/" prefetch="intent">
-          return home
+      <div style={{textAlign: 'center'}}>
+        <p>We're sorry, the page you're looking for doesn't exist</p>
+        <NavLink style={{textDecoration: 'underline'}} to="/" prefetch="intent">
+          Return home
         </NavLink>
       </div>
     </div>
