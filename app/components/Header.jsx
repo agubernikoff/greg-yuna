f0c348917366caff51aa0906e70cf3af39b01faf
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-06-12 00:34:47 -0400

    fix styling cookie

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -18,8 +18,12 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
     setTimeout(() => {
       setT(false);
     }, 1000);
+
     setTimeout(() => {
-      document.querySelector('#shopify-pc__banner').style.opacity = 1;
+      const banner = document.querySelector('#shopify-pc__banner');
+      if (banner) {
+        banner.style.opacity = 1;
+      }
     }, 2000);
   }, []);
   const {close} = useAside();
