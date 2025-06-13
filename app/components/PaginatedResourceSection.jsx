cab509b3d321823cd8d411f1ed628358990245b9
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-06-13 08:35:28 -0400

    refresh error

--- app/components/PaginatedResourceSection.jsx
+++ app/components/PaginatedResourceSection.jsx
@@ -1,55 +1,3 @@
-// import * as React from 'react';
-// import {Pagination} from '@shopify/hydrogen';
-
-// /**
-//  * <PaginatedResourceSection > is a component that encapsulate how the previous and next behaviors throughout your application.
-//  * @param {Class<Pagination<NodesType>>['connection']>}
-//  */
-// export function PaginatedResourceSection({
-//   connection,
-//   children,
-//   resourcesClassName,
-//   fillEmptySpaces = true,
-// }) {
-//   return (
-//     <Pagination connection={connection}>
-//       {({nodes, isLoading, PreviousLink, NextLink}) => {
-//         const resourcesMarkup = nodes.map((node, index) =>
-//           children({node, index}),
-//         );
-
-//         return (
-//           <div
-//             className="account-orders-grid"
-//             style={{
-//               display: 'grid',
-//               gridColumn: '1/5',
-//               gridTemplateColumns: 'subgrid',
-//             }}
-//           >
-//             <PreviousLink>
-//               {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
-//             </PreviousLink>
-//             {resourcesClassName ? (
-//               <div className={resourcesClassName}>
-//                 {resourcesMarkup}
-//                 {fillEmptySpaces && <GridPlaceholder products={nodes} />}
-//               </div>
-//             ) : (
-//               <>
-//                 {resourcesMarkup}
-//                 {fillEmptySpaces && <GridPlaceholder products={nodes} />}
-//               </>
-//             )}
-//             <NextLink>
-//               {isLoading ? 'Loading...' : <span>Load more ↓</span>}
-//             </NextLink>
-//           </div>
-//         );
-//       }}
-//     </Pagination>
-//   );
-// }
 import React, {useState, useEffect, useRef} from 'react';
 import {Pagination} from '@shopify/hydrogen';
 import {useNavigate, Link} from '@remix-run/react';
@@ -81,7 +29,10 @@ export function PaginatedResourceSection({
 
   function cleanFilterUrl(url) {
     if (!url) return '';
-    const parsed = new URL(url, window.location.origin);
+    const parsed =
+      typeof window !== 'undefined'
+        ? new URL(url, window.location.origin)
+        : new URL(url, 'http://localhost');
     const params = parsed.searchParams;
 
     const filter = params.get('filter');
