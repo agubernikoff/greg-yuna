0dcce4d28da8be36137a839a7d2ded46eb124da2
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-06-13 11:38:58 -0400

    cookies test

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,6 +17,7 @@ export default function Layout() {
   const data = useRouteLoaderData('root');
 
   console.log('Analytics consent prop:', data.consent);
+  console.log('Analytics full data payload:', data);
 
   return (
     <html lang="en">
